0323b022f2c0ca8352a7cf9751ff11b35e3f8dcb
Pun <impan_1611_2544@hotmail.com>
2024-01-23 12:00:00 +0700

    Create:Basic8.py(Enter widget)

---
+++ Basic8.py
@@ -0,0 +1,18 @@
+from tkinter import *
+
+# Create screen
+root = Tk()
+
+# Setting main screen
+root.title("My program")
+root.iconbitmap("icons/logo.ico")
+root.geometry("300x300+500+200")
+root.resizable(0, 0)
+
+# Entry Widget
+myText=Entry(root, width=40, font=('Arial', 30))
+myText.pack(padx=5, pady=10)
+btnSave=Button(root, text="save", fg="white", bg="blue")
+btnSave.pack(ipadx=30, ipady=10)
+
+root.mainloop()
