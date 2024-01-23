5612a5dd20b35531fe5ed04a03c5ab83b734924f
Pun <impan_1611_2544@hotmail.com>
2024-01-23 20:36:40 +0700

    open and read txt file

--- Basic10.py
+++ Basic10.py
@@ -10,9 +10,11 @@ root.geometry("300x300+500+200")
 root.resizable(0, 0)
 
 def selectFile():
-    fileOpen=askopenfilename(title="Open file", initialdir="./", filetypes=(("Text Files", "*.txt"), ("All Files", "*"))) # initialdir is set location to open file.
-    print(fileOpen)
+    myFile=askopenfilename(title="Open file", initialdir="./", filetypes=(("Text Files", "*.txt"), ("All Files", "*"))) # initialdir is set location to open file.
     
+    with open(myFile, "r", encoding="utf8") as f:
+        Label(root, text=f.read()).pack()
+
 btn1=Button(root, text="Select file", command=selectFile)
 btn1.pack()
 root.mainloop()
