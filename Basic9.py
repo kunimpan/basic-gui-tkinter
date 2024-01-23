9f937b41700644a9ae28913fd99498cea3aff62a
Pun <impan_1611_2544@hotmail.com>
2024-01-23 20:08:29 +0700

    Create:Basic9(RadioButton)

---
+++ Basic9.py
@@ -0,0 +1,22 @@
+from tkinter import *
+
+# Create screen
+root = Tk()
+
+# Setting main screen
+root.title("My program")
+root.iconbitmap("icons/logo.ico")
+root.geometry("300x300+500+200")
+root.resizable(0, 0)
+
+def selectChoice():
+    result = choice.get()
+    print(result)
+
+choice = IntVar()
+# Radio button
+Radiobutton(root, text="Green", value=1, variable=choice, command=selectChoice).grid(row=0, column=0)
+Radiobutton(root, text="Pink", value=2, variable=choice, command=selectChoice).grid(row=0, column=1)
+Radiobutton(root, text="Purple", value=3, variable=choice, command=selectChoice).grid(row=0, column=2)
+
+root.mainloop()
