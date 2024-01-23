f35aea8a1eae43cc8c05c1bea82614e6b8c0dbb9
Pun <impan_1611_2544@hotmail.com>
2024-01-23 19:52:41 +0700

    Quit program function

--- Basic8.py
+++ Basic8.py
@@ -10,6 +10,14 @@ root.iconbitmap("icons/logo.ico")
 root.geometry("300x300+500+200")
 root.resizable(0, 0)
 
+def quitProgram():
+    confirm = tkinter.messagebox.askquestion("Confrim", "Do you want to close the program ?")
+    if confirm == "yes":
+        root.destroy()
+    else:
+        pass
+        
+
 def showName():
     name = userName.get() # Get data form userName.
     if name == "":
@@ -18,11 +26,17 @@ def showName():
         myText.delete(0, END) # Clear text(start, end).
         tkinter.messagebox.showinfo("Detail", "Users = " + name) # title, content
 
+
+
 # Entry Widget
 userName = StringVar() # This part keeps data from Entry Widget
 myText=Entry(root, width=40, font=('Arial', 30), textvariable=userName) # Part display
 myText.pack(padx=5, pady=10)
 btnSave=Button(root, text="save", fg="white", bg="blue", command=showName)
 btnSave.pack(ipadx=30, ipady=10)
+
+# Create a button for exiting the program.
+btnQuit = Button(root, text="Exit the program", fg="white", bg="purple", command=quitProgram)
+btnQuit.pack(ipadx=30, ipady=10, pady=5)
 
 root.mainloop()
