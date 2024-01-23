63d686233d32b1f3969806de6e4e90e855bc0d1b
Pun <impan_1611_2544@hotmail.com>
2024-01-23 19:23:16 +0700

    StringVar

--- Basic8.py
+++ Basic8.py
@@ -10,12 +10,13 @@ root.geometry("300x300+500+200")
 root.resizable(0, 0)
 
 def showName():
-    name = myText.get() # Get data form myText.
+    name = userName.get() # Get data form userName.
     print(name)
     myText.delete(0, END) # Clear text(start, end).
 
 # Entry Widget
-myText=Entry(root, width=40, font=('Arial', 30))
+userName = StringVar() # This part keeps data from Entry Widget
+myText=Entry(root, width=40, font=('Arial', 30), textvariable=userName) # Part display
 myText.pack(padx=5, pady=10)
 btnSave=Button(root, text="save", fg="white", bg="blue", command=showName)
 btnSave.pack(ipadx=30, ipady=10)
