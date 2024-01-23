7d1e33ccce95616ee6544b7e2aedb87c515197ed
Pun <impan_1611_2544@hotmail.com>
2024-01-23 11:19:13 +0700

    Create:Basic6.py(grid layout)

---
+++ Basic6.py
@@ -0,0 +1,29 @@
+from tkinter import *
+
+# Create screen
+root = Tk()
+
+# Setting main screen
+root.title("My program")
+root.iconbitmap("icons/logo.ico")
+root.geometry("300x300+500+200")
+root.resizable(0, 0)
+root.config(bg="pink")
+
+btn1=Button(root, text="Btn 1")
+btn2=Button(root, text="Btn 2")
+btn3=Button(root, text="Btn 3")
+btn4=Button(root, text="Btn 4")
+btn5=Button(root, text="Btn 5")
+btn6=Button(root, text="Btn 6")
+
+# Use grid to alignment.
+btn1.grid(row=0, column=0, padx=5, pady=5, ipadx=10, ipady=10)
+btn2.grid(row=0, column=1, padx=5, pady=5, ipadx=10, ipady=10)
+btn3.grid(row=0, column=2, padx=5, pady=5, ipadx=10, ipady=10)
+
+btn4.grid(row=1, column=0, padx=5, pady=5, ipadx=10, ipady=10)
+btn5.grid(row=1, column=1, padx=5, pady=5, ipadx=10, ipady=10)
+btn6.grid(row=1, column=2, padx=5, pady=5, ipadx=10, ipady=10)
+
+root.mainloop()
