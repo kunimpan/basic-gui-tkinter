c2a85b9e5feb890f0325710aa8561b2e457c70c9
Pun <impan_1611_2544@hotmail.com>
2024-01-23 11:01:32 +0700

    Create:Basic.py(button widget)

---
+++ Basic5.py
@@ -0,0 +1,27 @@
+from tkinter import *
+
+# Create screen
+root = Tk()
+
+# Setting main screen
+root.title("My program")
+root.iconbitmap("icons/logo.ico")
+root.geometry("300x300+500+200")
+root.resizable(0, 0)
+root.config(bg="orange")
+
+# Create button
+btn1 = Button(root, text="Button one")
+btn1.pack(side=TOP)
+
+btn2 = Button(root, text="Button two", bg="red", fg="white")
+btn2.pack(side=LEFT)
+
+btn3 = Button(root, text="Button three", bg="black", fg="white")
+btn3.pack(side=RIGHT)
+
+btn4 = Button(root, text="Button four", bg="green", fg="white", activebackground="red", activeforeground="white")
+btn4.pack(side=BOTTOM)
+
+
+root.mainloop()
