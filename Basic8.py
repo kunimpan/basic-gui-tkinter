9d0940ab96af92fd5104b1a67e5d2217ece99ee5
Pun <impan_1611_2544@hotmail.com>
2024-01-23 12:11:25 +0700

    add:showName function

--- Basic8.py
+++ Basic8.py
@@ -9,10 +9,15 @@ root.iconbitmap("icons/logo.ico")
 root.geometry("300x300+500+200")
 root.resizable(0, 0)
 
+def showName():
+    name = myText.get() # Get data form myText.
+    print(name)
+    myText.delete(0, END) # Clear text(start, end).
+
 # Entry Widget
 myText=Entry(root, width=40, font=('Arial', 30))
 myText.pack(padx=5, pady=10)
-btnSave=Button(root, text="save", fg="white", bg="blue")
+btnSave=Button(root, text="save", fg="white", bg="blue", command=showName)
 btnSave.pack(ipadx=30, ipady=10)
 
 root.mainloop()
